636ef2ae928c614c73e80b359dea0db2d9c08cb5
kurholio <kurholio@gmail.com>
2025-08-29 11:31:01 -0500

    deploy script tweaks

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
   },
   css: {
     preprocessorOptions: {
